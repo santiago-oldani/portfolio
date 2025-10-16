0f147f6645f05aa59afbc559c0f3ab24bcd694f1
Santi543 <santioldani08@gmail.com>
2025-10-16 18:49:43 -0300

    ADD: Contact.tsx and backend folder with node.js

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
+
+
+app.use(express.json());
+
+app.post('/contact', async (req, res) =>{
+    const {fullname, email, message} = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    })
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: `Nuevo mensaje de ${fullname}`,
+        text: `Email: ${email}\n\nMensaje:\n${message}`
+    }
+
+    try{
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({success: true});
+    } catch (error){
+        console.error("Error al enviar el correo:", error);
+        res.status(500).json({success: false, error: error.message});
+    }
+
+    
+});
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`)
+})
